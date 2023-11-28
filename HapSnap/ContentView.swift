c2f054780312ac6619903abe9027936f6a7d33d5
Giovanni Palusa <giovanni@palusa.se>
2023-11-28 18:22:10 +0100

    Initial Commit

---
+++ HapSnap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HapSnap
+//
+//  Created by Giovanni Palusa on 2023-11-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HapSnap/HapSnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapSnapApp.swift
+//  HapSnap
+//
+//  Created by Giovanni Palusa on 2023-11-28.
+//
+
+import SwiftUI
+
+@main
+struct HapSnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
